1c014e8b3781dbb6dd59cd329a8757eb6417e8c8
mharry1997 <107109724+mharry1997@users.noreply.github.com>
2025-08-12 11:41:53 +0100

    Move local sync specific functions from utils to local_scan

--- src/local_scan.py
+++ src/local_scan.py
@@ -1,8 +1,60 @@
 import os
 from dotenv import load_dotenv
+import hashlib
+import pathlib
+from mutagen.easyid3 import EasyID3
+from mutagen.flac import FLAC
+from mutagen.mp3 import MP3
 
 from src.database import initialise_database
-from src.utils import get_local_track_info
+from src.utils import gen_hash
+
+
+# Generate unique hash from audio of file. Allows persistent id when metadata may change.
+def generate_audio_hash(filepath):
+  # Use a buffer size of 64k. This is efficient for reading files.
+  buf_size = 65536
+
+  sha256 = hashlib.sha256()
+
+  try:
+    with open(filepath, 'rb') as f:
+      while True:
+        data = f.read(buf_size)
+        if not data:
+          break
+        sha256.update(data)
+    return sha256.hexdigest()
+  except IOError:
+    # Return None if the file can't be read for some reason
+    return None
+
+# Get metadata for a locally stored track
+def get_local_track_info(file_path):
+  # Parse file path for suffix
+  file_type = pathlib.Path(file_path).suffix
+  if file_type == ".mp3":
+    audio = MP3(file_path, ID3=EasyID3)
+  elif file_type == ".flac":
+    audio = FLAC(file_path)
+  else:
+    print(f"Filetype for {file_path} not recognised")
+    return None
+
+  artist_list = audio.get('ARTIST')
+  artist = artist_list[0] if artist_list else None
+
+  album_list = audio.get('ALBUM')
+  album = album_list[0] if album_list else None
+
+  title_list = audio.get('TITLE')
+  name = title_list[0] if title_list else None
+
+  audio_hash = generate_audio_hash(file_path)
+  gen_id = gen_hash(artist,album,name)
+
+
+  return artist, gen_id, album, name, audio_hash, file_type
 
 
 def sync_local():

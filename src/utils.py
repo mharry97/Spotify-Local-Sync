1bcdafe1e47b3930cace307fe2a930364f5ce533
mharry1997 <107109724+mharry1997@users.noreply.github.com>
2025-08-13 09:43:00 +0100

    Turns out hash was deterministic

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,10 @@
+from hashlib import md5
+
 # Generate normalised hash from track information
 def gen_hash(artist, album, song):
   if artist and album and song:
     conc = artist + album + song
     key = conc.lower()
-    return hash(key)
+    return md5(key.encode()).hexdigest()
   else:
     return None
